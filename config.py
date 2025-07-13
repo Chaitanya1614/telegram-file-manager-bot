886c4ffcf77ca5ab50843eddcf89d899b863d9b2
Your Name <your-email@example.com>
2025-07-13 16:12:43 +0000

    Use BOT_TOKEN from secrets in config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 import os
 
-TELEGRAM_BOT_TOKEN = "7753804175:AAEkk9JV9-kXTY1EVY_WTNhlrClAtepCQ5k"
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Read from environment variable
 DOWNLOAD_BASE_DIR = "downloads"
-os.makedirs(DOWNLOAD_BASE_DIR, exist_ok=True)
